d20e7fc33d2675c889c88c043cc0928ff2be856f
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-28 20:48:12 +0300

    done

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,13 +6,24 @@ import {
   getContactsController,
   updateContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', getContactsController);
-router.get('/:contactId', getContactByIdController);
-router.post('/', createContactController);
-router.patch('/:contactId', updateContactController);
-router.delete('/:contactId', deleteContactController);
+router.get('/:contactId', isValidId, getContactByIdController);
+router.post('/', validateBody(createContactSchema), createContactController);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContactController,
+);
+router.delete('/:contactId', isValidId, deleteContactController);
 
 export default router;
